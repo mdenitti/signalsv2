6ccb42335ce6bd43543f9ab88262572c05539124
AMicciche <alessiomicciche@outlook.com>
2025-12-16 10:34:34 +0100

    feat: add Gemini AI service and integrate Google Generative AI package

---
+++ src/app/services/gemini-ai.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import {Environments} from '../../environments/environments';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GeminiAi {
+
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-environments',
+  imports: [],
+  templateUrl: './environments.html',
+  styleUrl: './environments.css',
+})
+export const Environments = {
+  production: false,
+  geminiApiKey: 'AIzaSyDJ1KXkFuqKSeeuDZKPbyXfF2Ydg6JmfEo'
+};
